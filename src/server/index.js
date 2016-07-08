458c6a4e8a51e802cf16323a1b8c448485b2bd1c
Dustin Arnold <dustin.arnold@compassphs.com>
2016-07-07 23:29:41 -0500

    project reorganization

---
+++ src/server/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let Hapi = require('hapi');
+
+let port = process.env.PORT || 3000;
+
+const server = new Hapi.Server();
+server.connection({ port: port });
+
+server.register(require('inert'), (err) => {
+
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'src/client'
+      }
+    }
+  });
+
+  server.start((err) => {
+
+    if (err) {
+      throw err;
+    }
+
+    console.log('Server running at:', server.info.uri);
+  });
+});
+
+require('./videos').registerApi(server);
