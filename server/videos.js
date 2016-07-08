3fc0d015c4188a2c5e3c1b8f1d4f5fb1d943c854
Dustin Arnold <dustin.arnold@compassphs.com>
2016-07-07 21:33:09 -0500

    broke project into client and server; moved client side to bower and server side to hapi

---
+++ server/videos.js
@@ -0,0 +1,83 @@
+'use strict'
+
+var monk = require('monk');
+var db = monk('192.168.99.100:32769/vidzy');
+
+let getall = {
+    method: 'GET',
+    path: '/api/videos',
+    handler: function (req, res) {
+        var collection = db.get('videos');
+        collection.find({}, function (err, videos) {
+            if (err) throw err;
+            res(videos);
+        });
+    }
+}
+
+let post = {
+    method: 'POST',
+    path: '/api/videos',
+    handler: function (req, res) {
+        var collection = db.get('videos');
+        collection.insert({
+            title: req.payload.title,
+            description: req.payload.description
+        }, function (err, video) {
+            if (err) throw err;
+            res(video);
+        })
+    }
+}
+
+let get = {
+    method: 'GET',
+    path: '/api/videos/{id}',
+    handler: function (req, res) {
+        var collection = db.get('videos');
+        collection.findOne({ _id: req.params.id }, function (err, video) {
+            if (err) throw err;
+            res(video);
+        });
+    }
+}
+
+let put = {
+    method: 'PUT',
+    path: '/api/videos/{id}',
+    handler: function (req, res) {
+        var collection = db.get('videos');
+        collection.update({
+            _id: req.params.id
+        },
+            {
+                title: req.payload.title,
+                description: req.payload.description
+            }, function (err, video) {
+                if (err) throw err;
+                res(video);
+            });
+    }
+}
+
+let destroy = {
+    method: 'DELETE',
+    path: '/api/videos/{id}',
+    handler: function (req, res) {
+        var collection = db.get('videos');
+        collection.remove({ _id: req.params.id }, function (err, video) {
+            if (err) throw err;
+            res(video);
+        });
+    }
+};
+
+module.exports = {
+    registerApi: function (server) {
+        server.route(getall);
+        server.route(get);
+        server.route(post);
+        server.route(put);
+        server.route(destroy);
+    }
+}
